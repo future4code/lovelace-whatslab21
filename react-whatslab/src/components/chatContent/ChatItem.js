a986fc2fca84fa795ddd6b1c35d33eeb0ab02998
Jesus Rios <jrios@electrodata.com.pe>
2021-07-21 11:59:33 -0500

    atualizado

--- react-whatslab/src/components/chatContent/ChatItem.js
+++ react-whatslab/src/components/chatContent/ChatItem.js
@@ -12,7 +12,7 @@ export default class ChatItem extends Component {
         className={`chat__item ${this.props.user ? this.props.user : ""}`}
       >
         <div className="chat__item__content">
-          <div className="chat__msg">{this.props.msg}</div>
+        <div className="chat__msg">{this.props.msg}</div>
           <div className="chat__meta">
             <span>16 mins ago</span>
             <span>Seen 1.03PM</span>
